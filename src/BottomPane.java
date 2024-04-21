a68e6fcf68b4f3cf9748bc8f525d504ad59eac0d
setenfa <yuanmingk7@outlook.com>
2024-04-21 16:32:13 +0800

    add BottomPane

---
+++ src/BottomPane.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class BottomPane extends JPanel{
+    private JLabel infoLabel;
+
+    public BottomPane(){
+        infoLabel = new JLabel();
+        infoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        add(infoLabel);
+    }
+
+    public void updateInfo(int totalImages, long totalSize, int selectedImages){
+        double sizeInMB = totalSize / 1024.0 / 1024.0;
+        infoLabel.setText(totalImages + "张图片(" + String.format("%.2f", sizeInMB) + "MB) - 选中" + selectedImages + "张图片");
+    }
+}
