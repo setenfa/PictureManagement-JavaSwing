a1f3138d8a91a2813f54f05e0c044be809e390e9
setenfa <yuanmingk7@outlook.com>
2024-04-22 19:08:15 +0800

    separate the JTree from the filePane

---
+++ src/util/ui/Tree.java
@@ -0,0 +1,11 @@
+package util.ui;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Tree extends JTree {
+    public Tree(DefaultMutableTreeNode root) {
+        super(root);
+    }
+
+}
