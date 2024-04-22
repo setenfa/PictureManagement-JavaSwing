9d4a02587caa732ac74b479b1d021b44205343ce
setenfa <yuanmingk7@outlook.com>
2024-04-22 19:06:49 +0800

    deal with conflict

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import ui.FilePane;
+import ui.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
-        FilePane fileNode = new FilePane();
-        JFrame frame = new JFrame("电子图片管理程序");
-        frame.add(fileNode.panel1, BorderLayout.WEST);
-        frame.add(fileNode.getImageDisplay().getScrollPane(), BorderLayout.CENTER);
-        frame.add(fileNode.getImageDisplay().getBottomPane(), BorderLayout.SOUTH);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        // 让frame在屏幕中居中显示
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        // 添加组件监听器
-        frame.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                // 当窗口大小改变时，强制重新布局
-                fileNode.getImageDisplay().getScrollPane().revalidate();
-                fileNode.getImageDisplay().getScrollPane().repaint();
-            }
-        });
+        FilePane filePane = new FilePane();
+        MainWindow mainWindow = new MainWindow(filePane, 800, 500);
     }
 }
