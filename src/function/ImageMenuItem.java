bb7fc4b5c1fb472dfc3db7872d1c81a0cf5df4bd
setenfa <yuanmingk7@outlook.com>
2024-04-25 16:47:52 +0800

    add the function of rename

--- src/function/ImageMenuItem.java
+++ src/function/ImageMenuItem.java
@@ -2,6 +2,7 @@ package function;
 import ui.ImageDisplay;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -48,11 +49,47 @@ public class ImageMenuItem {
             JOptionPane.showMessageDialog(null, "粘贴成功");
         });
         renameItem.addActionListener((ActionEvent e) -> {
-
+            if (imageDisplay.getSelectedImagePaths().size() == 1) {
+                String newName = JOptionPane.showInputDialog("请输入新的文件名");
+                if (newName == null) {
+                    return;
+                }
+                if (newName.trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "文件名不能为空");
+                    return;
+                }
+                renameImage(newName, 1, 1);
+                JOptionPane.showMessageDialog(null, "重命名成功");
+            } else {
+                JPanel panel = new JPanel(new GridLayout(3, 2));
+                JTextField prefixField = new JTextField();
+                JTextField startNumberField = new JTextField();
+                JTextField digitField = new JTextField();
+                panel.add(new JLabel("名称前缀: "));
+                panel.add(prefixField);
+                panel.add(new JLabel("起始编号: "));
+                panel.add(startNumberField);
+                panel.add(new JLabel("编号位数: "));
+                panel.add(digitField);
+                int result = JOptionPane.showOptionDialog(null, panel, "批量重命名",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+                if (result == JOptionPane.OK_OPTION) {
+                    String  prefix = prefixField.getText();
+                    if (prefix == null || prefix.trim().isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "文件名不能为空");
+                        return;
+                    }
+                    int startNumber = Integer.parseInt(startNumberField.getText());
+                    int digit = Integer.parseInt(digitField.getText());
+                    renameImage(prefix, startNumber, digit);
+                    JOptionPane.showMessageDialog(null, "重命名成功");
+                }
+            }
         });
         popupMenu.add(deleteItem);
         popupMenu.add(copyItem);
         popupMenu.add(pasteItem);
+        popupMenu.add(renameItem);
         imageDisplay.getImagePanel().addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -60,9 +97,11 @@ public class ImageMenuItem {
                     if (imageDisplay.getSelectedImagePaths().isEmpty()) {
                         deleteItem.setEnabled(false);
                         copyItem.setEnabled(false);
+                        renameItem.setEnabled(false);
                     } else {
                         deleteItem.setEnabled(true);
                         copyItem.setEnabled(true);
+                        renameItem.setEnabled(true);
                     }
                     pasteItem.setEnabled(!copiedImagePaths.isEmpty());
                     popupMenu.show(e.getComponent(), e.getX(), e.getY());
@@ -95,6 +134,38 @@ public class ImageMenuItem {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        imageDisplay.refreshImages();
+    }
+
+    // 重命名图片
+    public void renameImage(String prefix, int startNumber, int digit) {
+        List<String> selectedImagePathsCopy = new ArrayList<>(imageDisplay.getSelectedImagePaths());
+        int counter = startNumber;
+        for(String path : selectedImagePathsCopy) {
+            Path oldPath = Paths.get(path);
+            String extension = "";
+            int i = path.lastIndexOf('.');
+            if (i > 0) {
+                extension = path.substring(i + 1);
+            }
+            String newName;
+            if (selectedImagePathsCopy.size() > 1) {
+                String format = "%0" + digit + "d";
+                newName = prefix + String.format(format, counter);
+            } else {
+                newName = prefix;
+            }
+            newName += "." + extension;
+            Path newPath = oldPath.resolveSibling(newName);
+            try {
+                Files.move(oldPath, newPath);
+                int index = imageDisplay.getSelectedImagePaths().indexOf(path);
+                imageDisplay.getSelectedImagePaths().set(index, newPath.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            counter++;
         }
         imageDisplay.refreshImages();
     }
