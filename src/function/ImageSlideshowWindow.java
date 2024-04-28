c52d0915995e0150e1f2f5e89efa3449f953b01c
setenfa <yuanmingk7@outlook.com>
2024-04-28 21:56:06 +0800

    add the function

---
+++ src/function/ImageSlideshowWindow.java
@@ -0,0 +1,72 @@
+package function;
+
+import ui.ImageDisplay;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ImageSlideshowWindow extends JFrame {
+    private ImageDisplay imageDisplay;
+    // 记录arraylist下标
+    private int index;
+    // 存放图片
+    private JLabel imageLabel;
+
+    public ImageSlideshowWindow(int index, ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+        this.index = index;
+        this.imageLabel = new JLabel();
+        // 前进、后退、放大、缩小、自动播放按钮
+        JToolBar toolBar = initToolBar();
+        add(imageLabel, "Center");
+        add(toolBar, "South");
+        // 全屏
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public JToolBar initToolBar() {
+        JButton previousButton = new JButton("<--");
+        JButton nextButton = new JButton("-->");
+        JButton zoomInButton = new JButton("放大");
+        JButton zoomOutButton = new JButton("缩小");
+        JButton autoPlayButton = new JButton("自动播放");
+        JToolBar toolB = new JToolBar();
+        previousButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                index--;
+                if (index < 0) {
+                    JOptionPane.showMessageDialog(null, "已经是第一张图片了");
+                }
+            }
+        });
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                index++;
+                if (index > imageDisplay.getSmallPanels().size()) {
+                    JOptionPane.showMessageDialog(null, "已经是最后一张图片了");
+                } else {
+                    showImage();
+                }
+            }
+        });
+        toolB.add(previousButton);
+        toolB.add(nextButton);
+        toolB.add(zoomInButton);
+        toolB.add(zoomOutButton);
+        toolB.add(autoPlayButton);
+        return toolB;
+    }
+
+    public void showImage() {
+        Icon imageIcon = imageDisplay.getSmallLabels().get(index).getIcon();
+        imageLabel.setIcon(imageIcon);
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER); // 设置水平居中
+        imageLabel.setVerticalAlignment(SwingConstants.CENTER); // 设置垂直居中
+        index++;
+    }
+}
