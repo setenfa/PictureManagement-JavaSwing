8c1d2c8e09b862fdca8e2e2e9b20460245c30d50
setenfa <yuanmingk7@outlook.com>
2024-05-09 14:25:08 +0800

    增设一个进入幻灯片播放的入口

--- src/ui/BottomPane.java
+++ src/ui/BottomPane.java
@@ -1,13 +1,31 @@
 package ui;
 
+import function.ImageSlideshowWindow;
+
 import javax.swing.*;
+import java.awt.*;
+
 public class BottomPane extends JPanel{
     private JLabel infoLabel;
+    private final ImageDisplay imageDisplay;
+    private JButton ImageSlideshowWindowButton;
 
-    public BottomPane(){
+    public BottomPane(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+        setLayout(new BorderLayout());
         infoLabel = new JLabel();
+        ImageSlideshowWindowButton = new JButton("\ue768");
+        ImageSlideshowWindowButton.setFont(new Font("Segoe MDL2 Assets", Font.PLAIN, 18));
+        ImageSlideshowWindowButton.addActionListener(e -> {
+            if (imageDisplay.getOriginalIcons().size() == 0) {
+                JOptionPane.showMessageDialog(null, "该文件夹暂无图片，无法播放");
+            } else {
+                new ImageSlideshowWindow(0, imageDisplay).showImage();
+            }
+        });
         infoLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        add(infoLabel);
+        add(infoLabel, BorderLayout.WEST);
+        add(ImageSlideshowWindowButton, BorderLayout.EAST);
         infoLabel.setText("0张图片(0MB) - 选中0张图片");
     }
 
